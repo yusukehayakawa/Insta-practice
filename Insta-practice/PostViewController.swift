73fc18bb1ae8a88ee13c1aae652234a037827423
HayakawaYusuke <yusukeeee0412@gmail.com>
2018-04-10 14:17:07 +0900

    first commit

---
+++ Insta-practice/PostViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostViewController.swift
+//  Insta-practice
+//
+//  Created by HayakawaYusuke on 2018/04/10.
+//  Copyright © 2018年 HayakawaYusuke. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
