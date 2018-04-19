e7ba312bca36495517b9c757e218b30f53cac7aa
HayakawaYusuke <yusukeeee0412@gmail.com>
2018-04-19 19:18:20 +0900

    add handle login button method

--- Insta-practice/LoginViewController.swift
+++ Insta-practice/LoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
 
@@ -19,6 +20,32 @@ class LoginViewController: UIViewController {
     
     // ログインボタンをタップしたときに呼ばれるメソッド
     @IBAction func handleLoginButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text {
+
+// アドレスとパスワード名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty {
+                SVProgressHUD.showError(withStatus: "必須項目を入力してください")
+                return
+            }
+            // HUDで処理中を表示
+            SVProgressHUD.show()
+
+            Auth.auth().signIn(withEmail: address, password: password) { user, error in 
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "サインインに失敗しました。")
+                    return
+                } else {
+                    print("DEBUG_PRINT: ログインに失敗しました")
+
+                    // HUDを消す　
+                    SVProgressHUD.dismiss()
+
+                    // 画面を閉じてViewControllerに戻る
+                    self.dismiss(animated: true, completion: nil)
+                }
+             }
+        }
     }
     
     // アカウント作成ボタンをタップしたときに呼ばれるメソッド
@@ -28,14 +55,19 @@ class LoginViewController: UIViewController {
             // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
             if address.isEmpty || password.isEmpty || displayName.isEmpty {
                 print("DEBUG_PRINT: 何かが空文字です。")
+                SVProgressHUD.showError(withStatus: "必須項目を入力してください")
                 return
             }
+
+            // HUDで処理中を表示
+            SVProgressHUD.show()
 
             // アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする
             Auth.auth().createUser(withEmail: address, password: password) { user, error in
                 if let error = error {
                 // エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
                     print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "ユーザー作成に失敗しました。")
                     return
                 }
                 print("DEBUG_PRINT: ユーザー作成に成功しました")
@@ -48,8 +80,13 @@ class LoginViewController: UIViewController {
                     changeRequest.commitChanges { error in
                         if let error = error {
                             print("DEBUG_PRINT: " + error.localizedDescription)
+                            SVProgressHUD.showError(withStatus: "ユーザー作成時にエラーが発生しました。")
                         }
                         print("DEBUG_PRINT: [displayName = \(String(describing: user.displayName))]の設定に成功しました。")
+
+                            // HUDを消す
+                            SVProgressHUD.show()
+
                         // 画面を閉じてViewControllerに戻る
                         self.dismiss(animated: true, completion: nil)
                     }
